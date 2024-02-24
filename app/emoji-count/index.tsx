23d3cd878d1ee13c19e0545cd0c7ba573047d850
wangwei <w1220w@126.com>
2024-02-24 21:54:06 +0800

    feat: phone number generator

---
+++ app/emoji-count/index.tsx
@@ -0,0 +1,27 @@
+import { Suspense } from "react"
+
+interface CountDisplayProps {
+  count?: number
+}
+
+function CountDisplay({ count }: CountDisplayProps) {
+  return (
+    <p className="text-gray-500 mb-12 text-base animate-in fade-in slide-in-from-bottom-4 duration-1200 ease-in-out">
+      {count || "–––"} emojis generated and counting!
+    </p>
+  )
+}
+
+async function AsyncEmojiCount() {
+  const count = 100
+
+  return <CountDisplay count={count} />
+}
+
+export function EmojiCount() {
+  return (
+    <Suspense fallback={<CountDisplay />}>
+      <AsyncEmojiCount />
+    </Suspense>
+  )
+}
