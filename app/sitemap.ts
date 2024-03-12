9501380f27805b791075c8d4e7d624b54c25b430
Thomas Wade <w1220w@126.com>
2024-03-12 09:00:13 +0800

    feat: sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,13 +1,18 @@
 import { MetadataRoute } from 'next'
- 
+import { locales } from './const'
+const generate = (url: string, num?: string): MetadataRoute.Sitemap[number] => {
+  return {
+    'url': url + (num === undefined ? '' : `/${num}`),
+    lastModified: new Date(),
+    changeFrequency: 'monthly',
+    priority: 0.8,
+  }
+}
 export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: 'https://www.phonenumbergenerator.app',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 1,
-    },
-    
-  ]
+  const toGenerate = locales?.slice(0, 2)
+  const basePath = `https://www.phonenumbergenerator.app`;
+  const arr: MetadataRoute.Sitemap = [generate(basePath)].concat(toGenerate?.map(item => {
+    return generate(basePath, item.label)
+  }))
+  return arr
 }
