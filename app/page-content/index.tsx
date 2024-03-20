1701afb839934bf83e82d529027154d1d6513529
Thomas Wade <w1220w@126.com>
2024-03-20 12:38:02 +0800

    feat: phone number

--- app/page-content/index.tsx
+++ app/page-content/index.tsx
@@ -12,11 +12,12 @@ interface PageContentProps extends React.PropsWithChildren {
 
 export const PageContent = (props: PageContentProps) => {
   const { children, country } = props;
+  const countryName = locales.filter(item => item.label === country)?.[0]?.localeName || country?.replace('_phonenumber', '')
   return (
     <>
       <div className="py-[15vh] sm:py-[20vh] flex flex-col items-center justify-center">
         <h2 className="font-medium text-2xl md:text-4xl text-black mb-3 animate-in fade-in slide-in-from-bottom-3 duration-1000 ease-in-out">
-          Generate a Phone Number{props.isHome ? `` : ` for ${country?.replace('_phonenumber', '')}`} with Just a Click!
+          Generate a Phone Number{props.isHome ? `` : ` for ${countryName}`} with Just a Click!
         </h2>
         
         <h3 className="text-gray-500 mb-12 text-base animate-in fade-in slide-in-from-bottom-4 duration-1200 ease-in-out">
