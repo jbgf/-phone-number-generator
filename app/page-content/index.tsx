db2aa984e072ca2227d7c904a17b9401b0c916da
Thomas Wade <w1220w@126.com>
2024-03-26 09:03:40 +0800

    feat: image

--- app/page-content/index.tsx
+++ app/page-content/index.tsx
@@ -4,7 +4,8 @@ import { CountryLabels, locales } from "../const"
 import { EmojiCount } from "../emoji-count"
 import { EmojiForm } from "../emoji-form"
 import { UserGuide } from "../user-guide"
-
+import Image from "next/image"
+import Benifit from '../images/content/DSC00229-tinypng.jpg'
 interface PageContentProps extends React.PropsWithChildren {
   country: CountryLabels
   isHome?: boolean
@@ -27,13 +28,27 @@ export const PageContent = (props: PageContentProps) => {
         <div className="max-w-md space-y-4 w-full animate-in fade-in slide-in-from-bottom-4 duration-1200 ease-in-out">
           <EmojiForm country={country} />
         </div>
-        <section className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4 place-items-center pt-10">
+        <section className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-8 gap-4 place-items-center pt-10">
           {locales?.filter(item => item.label !== country).map(item => {
             return <Link key={item.label} className="whitespace-nowrap hover:text-sky-500 underline" href={`/${item.label}`
             } title={`${item.localeName} phone number generator`}>{item.localeName}</Link>
           })}
         </section>
-        <div className="pt-36"><UserGuide isHome={props.isHome} /></div>
+        <div className="pt-12">
+        <Image
+            // fill
+            alt="Generate numbers for testing and privacy"
+            src={Benifit}
+            // src={"/content/DSC00229-tinypng.jpg"}
+            // width={undefined}
+            sizes="object-fill"
+            // sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          />
+          </div>
+        <div className="pt-24">
+          
+          <UserGuide isHome={props.isHome} />
+        </div>
       </div>
 
       {/* <Suspense>
