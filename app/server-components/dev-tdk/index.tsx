f016603cf39902b39ef2609a3fb7b4bb8279610f
Thomas Wade <w1220w@126.com>
2024-05-21 08:55:55 +0800

    feat: fix build

--- app/server-components/dev-tdk/index.tsx
+++ app/server-components/dev-tdk/index.tsx
@@ -1,9 +1,10 @@
 "use client";
 export default function DevTKD () {
   const isDev = process.env.NODE_ENV === "development"
+  if (!isDev) return null
   const params = new URLSearchParams(window.location.search);
   const mode = params.get('mode');
-  if (!isDev || mode !== 'tkd') return null
+  if (mode !== 'tkd') return null
 
   return (
     <div className="absolute top-0 right-0 z-50 bg-gray-400 p-2 ">
