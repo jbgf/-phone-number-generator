49e738745fe167b4944cd39858f4970286883fe5
wangwei <w1220w@126.com>
2024-11-16 14:56:25 +0800

    feat: build

--- app/server-components/no-follow-link/index.tsx
+++ app/server-components/no-follow-link/index.tsx
@@ -1,15 +1,16 @@
 import Link from "next/link"
 
 import React, { useState, useEffect } from 'react';
-import cn from 'classnames/bind';
+import cn from 'classnames';
 import { Url } from "next/dist/shared/lib/router/router";
 interface IProps {
   href: Url
   children: React.ReactNode
+  className?: string
 }
 function NoFollowLink(props: IProps) {
   return (
-    <Link className="text-blue-600 visited:text-purple-600" href={props.href} target="_blank" rel="nofollow" >
+    <Link className={cn("text-blue-600 visited:text-purple-600", props.className)} href={props.href} target="_blank" rel="nofollow" >
       {props.children}
     </Link>
   )
