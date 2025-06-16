55fa2169bdbfb5fb74895784f09ed03edfc36a2a
wangwei <w1220w@126.com>
2025-06-16 12:59:52 +0800

    chore: update ESLint configuration to disable react/no-unescaped-entities rule and enhance layout script tag with clarity-script ID

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,7 +75,7 @@ export default function RootLayout({
           {/* <Providers /> */}
       </body>
       {shouldLoadAnalytics && <>
-        <Script type="text/javascript">
+        <Script type="text/javascript" id="clarity-script">
           {`(function(c,l,a,r,i,t,y){
             c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
             t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
